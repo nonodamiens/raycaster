8d5e1207fc27faf477f3b4f4d2e7a7aca4cda1ba
nonodamiens <arnaud.mercier80@gmail.com>
2021-07-04 16:06:49 +0200

    Added a blue line to simulate the player view direction

--- raycaster.py
+++ raycaster.py
@@ -39,6 +39,8 @@ for i, row in enumerate(environement):
 			canvas.create_rectangle(j*10+1,i*10+1,(j+1)*10+1,(i+1)*10+1, fill="red")
 		if v == "p":
 			canvas.create_oval(j*10+1,i*10+1,(j+1)*10+1,(i+1)*10+1, fill="white")
+			canvas.create_line(j*10+6,i*10+1,j*10+6,(i+1)*10+1, fill="blue")
+
 canvas.pack()
 
 window.mainloop() # Event loop (wait for user action) necessary for a script to show window
