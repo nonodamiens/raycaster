6c55877e9cdeed941f2949150adddf83a4e6c1ee
nonodamiens <nonodamiens@gmail.com>
2022-09-18 18:56:03 +0200

    added a line to show player direction

--- raycaster.py
+++ raycaster.py
@@ -18,18 +18,21 @@ environement = ["1111111111",
 				"1000000001",
 				"1111111111",]
 
+from math import pi # needed for angle calculus
+
 # We also need a player
 # let's make a class for it
 class Player:
 	"""Player's characteristics
-	The player has a position and a method to change position"""
+	The player has a position and a direction (angle)"""
 	def __init__(self):
-		self.x = 50
-		self.y = 50
+		self.x = 100
+		self.y = 100
+		self.d = 3 * pi / 2 # equivalent to 270° direction up
 
-	def move(self, dx, dy):
-		self.x = self.x + dx
-		self.y = self.y + dy
+	# def move(self, dx, dy):
+	# 	self.x = self.x + dx
+	# 	self.y = self.y + dy
 
 # ok now let's show graphicaly a map of our environement
 # We need a graphical library
@@ -45,12 +48,13 @@ canvas = tk.Canvas()
 canvas.configure(bg='black', width=200, height=200)
 for i, row in enumerate(environement):
 	for j, v in enumerate(row):
-		# print("coordinates :", i, j, v)
 		if int(v):
-			canvas.create_rectangle(j*20+1,i*20+1,(j+1)*20+1,(i+1)*20+1, fill="red")
-		# if v == "p":
-		# 	canvas.create_oval(j*10+1,i*10+1,(j+1)*10+1,(i+1)*10+1, fill="white")
-		# 	canvas.create_line(j*10+6,i*10+1,j*10+6,(i+1)*10+1, fill="blue")
+			canvas.create_rectangle(j*20+1,i*20+1,(j+1)*20+1,(i+1)*20+1, fill="red") # +1 to add a thin transparent line between square
+
+# Add a player to the scene
+p1 = Player() # init player
+canvas.create_oval((p1.x-3, p1.y-3), (p1.x+3, p1.y+3), fill="yellow")
+
 
 canvas.pack()
 
